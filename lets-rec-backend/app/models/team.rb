f41872e3c6288cb0701d6d764d2cf4d5b7286efc
Andrew-J-Williams <1williamsaj@gmail.com>
2020-10-20 18:54:47 -0500

    added relationships to models

--- lets-rec-backend/app/models/team.rb
+++ lets-rec-backend/app/models/team.rb
@@ -1,2 +1,5 @@
 class Team < ApplicationRecord
+    has_many :posts
+    has_many :user_teams
+    has_many :users, through: :user_teams
 end
--- lets-rec-backend/app/models/user.rb
+++ lets-rec-backend/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    has_many :posts
+    has_many :user_teams
+    has_many :teams, through: :user_teams
 end
