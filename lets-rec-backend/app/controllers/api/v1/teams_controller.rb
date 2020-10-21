a096ea862e2213e4b4c9589f13a81017ff2c1263
Andrew-J-Williams <1williamsaj@gmail.com>
2020-10-21 18:48:12 -0500

    completed methods for Teams controller

--- lets-rec-backend/app/controllers/api/v1/teams_controller.rb
+++ lets-rec-backend/app/controllers/api/v1/teams_controller.rb
@@ -1,29 +1,47 @@
 class Api::V1::TeamsController < ApplicationController
 
-    def index
+    before_action :set_team, only: [:show, :update, :destroy]
 
+    def index
+        @teams = Team.all
+
+        render json: @teams
     end
 
     def show
-
+        render json: @team
     end
 
     def create
+        @team = Team.new(team_params)
 
+        if @team.save
+            render json: @team
+        else
+            render json: {error: 'We could not process your team at this time.'}
+        end
     end
 
     def update
-
+        if @team.save
+            render json: @team
+        else
+            render json: {error: 'We could not modify your team at this time.'}
+        end
     end
 
     def destroy
-
+        @team.destroy
     end
 
     private
 
     def team_params
         params.require(:team).permit(:id, :name, :sport, :location, :venue, :timeslot, :status, :members)
+    end
+
+    def set_team
+        @team = Team.find(params[:id])
     end
 
 end
