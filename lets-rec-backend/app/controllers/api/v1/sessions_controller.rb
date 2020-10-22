781a07b2a810b2c4daae0bc3e1cb7b549c3923b8
Andrew-J-Williams <1williamsaj@gmail.com>
2020-10-21 19:32:13 -0500

    completed methods for Session and User controller

--- lets-rec-backend/app/controllers/api/v1/sessions_controller.rb
+++ lets-rec-backend/app/controllers/api/v1/sessions_controller.rb
@@ -1,15 +1,29 @@
 class Api::V1::SessionsController < ApplicationController
 
     def create
-
+        def create
+            @user = User.find_by(username: params[:session][:username])
+            session[:user_id] = @user.id 
+    
+            if @user && @user.authenticate(params[:session][:password])
+                render json: @user
+             else 
+                render json: {error: 'Failed Log In!'}
+            end 
     end
 
     def find_current_user
-
+        if logged_in?
+            render json: current_user 
+        else
+            render json: {error: 'Access Denied. Please Log In.'}
+        end
     end
 
     def destroy
+        session.clear
 
+        render json: {notice: 'Logged Out'}
     end
 
 end
--- lets-rec-backend/app/controllers/api/v1/users_controller.rb
+++ lets-rec-backend/app/controllers/api/v1/users_controller.rb
@@ -13,7 +13,14 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def create
-
+        @user = User.new(user_params)
+        
+        if @user.save
+            session[:user_id] = @user.id
+            render json: @user
+        else
+            render json: {error: 'Invalid User Credentials.'}
+        end
     end
 
 
