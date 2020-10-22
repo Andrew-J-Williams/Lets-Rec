dbfe3185599da3797e44bff3950a8c1220fe491f
Andrew-J-Williams <1williamsaj@gmail.com>
2020-10-21 19:12:09 -0500

    completed methods for Post controller

--- lets-rec-backend/app/controllers/api/v1/posts_controller.rb
+++ lets-rec-backend/app/controllers/api/v1/posts_controller.rb
@@ -1,29 +1,47 @@
 class Api::V1::PostsController < ApplicationController
 
-    def index
+    before_action :set_post, only: [:show, :update, :destroy]
 
+    def index
+        @posts = Post.all
+        
+        render json: @posts
     end
 
     def show
-
+        render json: @post
     end
 
     def create
+        @post = Post.new(post_params)
 
+        if @post.save
+            render json: @post
+        else
+            render json: {error: 'We could not process your post at this time.'}
+        end
     end
 
     def update
-
+        if @post.save
+            render json: @post
+        else
+            render json: {error: 'We could not update your post at this time.'}
+        end
     end
 
     def destroy
-
+        @post.destroy
     end
 
     private
 
     def post_params
         params.require(:post).permit(:id, :content, :user_id, :team_id)
+    end
+
+    def set_post
+        @post = Post.find(params[:id])
     end
 
 end
