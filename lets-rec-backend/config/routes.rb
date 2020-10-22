c2ff722be3c29e77c9479bed8b27ecae5e941dea
Andrew-J-Williams <1williamsaj@gmail.com>
2020-10-21 19:42:49 -0500

    modifying routes, adding routes for sessions controller

--- lets-rec-backend/config/routes.rb
+++ lets-rec-backend/config/routes.rb
@@ -1,11 +1,24 @@
 Rails.application.routes.draw do
 
+  post "api/v1/login", to: "api/v1/sessions#create"
+  post "/api/v1/signup", to: "api/v1/users#create"
+  get "api/v1/find_current_user", to: "api/v1/sessions#find_current_user"
+  delete "api/v1/logout", to: "api/v1/sessions#destroy"
+
   namespace :api do
     namespace :v1 do
+      
       resources :posts
       resources :user_teams
-      resources :teams
-      resources :users
+
+      resources :teams do
+        resources :users
+      end
+
+      resources :users do 
+        resources :teams
+      end
+
     end
   end
 
