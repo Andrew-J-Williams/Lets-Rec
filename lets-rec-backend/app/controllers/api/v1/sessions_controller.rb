3aaa8a43e3519647037a3df0435a8b05d21215ee
Andrew-J-Williams <1williamsaj@gmail.com>
2020-10-21 19:07:08 -0500

    completed methods in UserTeams controller, working on User, created Sessions controller

---
+++ lets-rec-backend/app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::SessionsController < ApplicationController
+
+    def create
+
+    end
+
+    def find_current_user
+
+    end
+
+    def destroy
+
+    end
+
+end
--- lets-rec-backend/app/controllers/api/v1/user_teams_controller.rb
+++ lets-rec-backend/app/controllers/api/v1/user_teams_controller.rb
@@ -1,30 +1,47 @@
 class Api::V1::UserTeamsController < ApplicationController
 
-    def index
+    before_action :set_user_team, only: [:show, :update, :destroy]
 
+    def index
+        @user_teams = UserTeam.all
+
+        render json: @user_teams
     end
 
     def show
-
+        render json: @user_teams
     end
 
     def create
+        @user_team = UserTeam.new(user_team_params)
 
+        if @user_team.save
+            render json: @user_team
+        else
+            render json: {error: 'We could not process your team at this time.'}
+        end
     end
 
     def update
-
+        if @user_team.save
+            render json: @user_team
+        else
+            render json: {error: 'We could not modify your team at this time.'}
+        end
     end
 
     def destroy
-
+        @user_team.destroy
     end
 
     private
 
-    def post_params
-        params.require(:post).permit(:id, :user_id, :team_id)
+    def user_team_params
+        params.require(:user_team).permit(:id, :user_id, :team_id)
     end
 
+    def set_user_team
+        @user_team = UserTeam.find(params[:id])
+    end
 
 end
