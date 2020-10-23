081f7da528b0417d0b525f77e46cbfacb03a69c0
Andrew-J-Williams <1williamsaj@gmail.com>
2020-10-22 19:55:51 -0500

    added serializer folder and files

--- lets-rec-backend/config/routes.rb
+++ lets-rec-backend/config/routes.rb
@@ -8,15 +8,16 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       
-      resources :posts
       resources :user_teams
 
       resources :teams do
         resources :users
+        resources :posts
       end
 
       resources :users do 
         resources :teams
+        resources :posts
       end
 
     end
--- lets-rec-backend/db/seeds.rb
+++ lets-rec-backend/db/seeds.rb
@@ -1,1 +1,4 @@
-team = Team.create(name: 'BossNation', sport: 'Basketball', location: 'Houston', venue: 'Downtown YMCA', timeslot: '5:00PM - 8:00PM', status: 'open', members: 7)
+andy = User.create(username: 'andyman', email: 'iamtheandyman94', password_digest: 'trymedude')
+
+
+andy.teams.create({name: 'BossNation', sport: 'Basketball', location: 'Houston', venue: 'Downtown YMCA', timeslot: '5:00PM - 8:00PM', status: 'open', members: 7})
