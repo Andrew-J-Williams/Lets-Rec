79a819961c1d188666c675ed7799078ddbe6f6c1
Andrew-J-Williams <1williamsaj@gmail.com>
2020-10-24 10:24:15 -0500

    building out store

--- lets-rec-frontend/src/index.js
+++ lets-rec-frontend/src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 
 import App from './App';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let reducer = ''
+
 let store = createStore(reducer,composeEnhancers(applyMiddleware(thunk)))
 
+// store is where we store our data globally.
+// reducers tell us what we want to do with our store based on certain actions. 
+// They receive action objects and decide what we want to update about our current store, and the reducer will return a new version.
+
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App/>
   </Provider>,
   document.getElementById('root')
