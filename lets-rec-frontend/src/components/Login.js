0082abec1164721cded15c83e244ff83748aa6a6
Andrew-J-Williams <1williamsaj@gmail.com>
2020-10-28 20:03:35 -0500

    working on event functions that handle state change and form submission

--- lets-rec-frontend/src/components/Login.js
+++ lets-rec-frontend/src/components/Login.js
@@ -6,17 +6,39 @@ import {logUserIn} from '../actions/loginUser'
 
 class Login extends React.Component {
 
+    state = {
+        username: '',
+        password: ''
+    }
+
+    handleOnChange = (event) => {
+        this.setState({
+            username: event.target.value,
+            password: event.target.value
+        })
+    }
+
+    handleOnSubmit = (event) => {
+        event.preventDefault()
+
+
+    }
+
     render(){
 
         return(
             <div>
-                <form>
+                <form onSubmit={this.handleOnSubmit}>
                     <input
                     placeholder="Username"
+                    value={this.state.username}
+                    onChange={this.handleOnChange}
                     ></input>
                     <input
                     placeholder="Password"
                     type="password"
+                    value={this.state.password}
+                    onChange={this.handleOnChange}
                     ></input>
                     <button
                     type="submit"
