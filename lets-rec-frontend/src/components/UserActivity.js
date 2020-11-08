82be76e56f87f0bcbb87308b401385acf8fdabc3
Andrew-J-Williams <1williamsaj@gmail.com>
2020-11-08 14:43:41 -0600

    incorporating user posts on activity feed, styling UserPosts

--- lets-rec-frontend/src/components/UserActivity.js
+++ lets-rec-frontend/src/components/UserActivity.js
@@ -1,13 +1,19 @@
 import React from 'react';
 
+import '../UserActivity.css'
+
 class UserActivity extends React.Component {
 
 
     render(){
         return(
-            <div classname="activity-contaier">
+            <div className="activity-container">
+                <h3>Activity Feed</h3>
                 {this.props.activity.map(post =>
-                    <h1>{post.content}</h1>
+                    <div key={post.id} className="user-post-container">
+                        <h5 className="post-username">{post.user.username}</h5>
+                        <p className="post-content">{post.content}</p>
+                    </div>
                 )}
             </div>
         )
