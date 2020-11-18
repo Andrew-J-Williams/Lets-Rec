25fee8cbe82d1dd5d34bad7cfaf1b375dd7244be
Andrew-J-Williams <1williamsaj@gmail.com>
2020-11-17 23:26:39 -0600

    created PostReducer

---
+++ lets-rec-frontend/src/reducers/postReducer.js
@@ -0,0 +1,18 @@
+const initialState = {
+    post: {
+        content:'',
+        user_id: 0,
+        team_id: 0
+    }
+}
+
+export default function postReducer (state=initialState, action) {
+
+    switch(action.type){
+        case 'CREATE_POST':
+        return{
+            post: action.payload
+        }
+    }
+
+}
