c8055d8d9b4e12b1c86202bf08e976d1ef72c7a0
Andrew-J-Williams <1williamsaj@gmail.com>
2020-11-18 20:38:27 -0600

    building out PostCreator

--- lets-rec-frontend/src/components/PostCreator.js
+++ lets-rec-frontend/src/components/PostCreator.js
@@ -3,10 +3,36 @@ import { connect } from 'react-redux';
 
 class PostCreator extends React.Component{
 
+
+    state = {
+        content: ''
+    }
+
+    handleOnChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+        console.log(event.target.value)
+    }
+
+    handleOnSubmit = (event) => {
+        event.preventDefault()
+        
+    }
+
     render(){
         return(
             <div>
-                <h1>TESTING POST CREATOR</h1>
+                <form onSubmit={this.handleOnSubmit}>
+                    <input
+                    name = "content"
+                    value={this.state.content}
+                    onChange={this.handleOnChange}
+                    ></input>
+                    <button
+                    type="submit"
+                    >Post</button>
+                </form>
             </div>
         )
     }
