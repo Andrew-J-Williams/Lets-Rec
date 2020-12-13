9694014d9a65b91a3d29f5db3538f6d65fa66c38
Andrew-J-Williams <1williamsaj@gmail.com>
2020-12-13 00:36:25 -0600

    added filter functionality to search under TeamFinder

--- lets-rec-frontend/src/components/FindTeams.js
+++ lets-rec-frontend/src/components/FindTeams.js
@@ -9,29 +9,53 @@ class FindTeams extends React.Component {
         sport:''
     }
 
-    handleInputChange = event => {
+    handleInputChange = (event) => {
         this.setState({[event.target.name]: event.target.value})
     }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+
+        const resultsContainer = document.getElementById("search-results")
+
+    }
+
 
     render(){
         const myArray = this.props.teams
         console.log(this.state)
+
+        let searchTeams = myArray.filter(team => team.location.toLowerCase().includes(this.state.location.toLowerCase()) && team.sport.toLowerCase().includes(this.state.sport.toLowerCase()))
+        console.log(searchTeams)
+
         return(
             <div className="find-teams-container">
                 <h2>Find a Team!</h2>
-                <label for="locations">Choose a Location: </label>
-                <select name="location" id="location-search" onChange={this.handleInputChange}>
-                    {myArray.map(team => 
-                        <option value={team.location} key={team.id}>{team.location}</option>    
-                    )}
-                </select>
-                <label>Choose a Sport: </label>
-                <select name="sport" onChange={this.handleInputChange}>
-                {myArray.map(team => 
-                        <option value={team.sport} key={team.id}>{team.sport}</option>    
-                    )}
-                </select>
-                <button type="submit">Search</button>
+                <form onSubmit={this.handleSubmit}>
+                    <label for="locations">Choose a Location: </label>
+                    <select name="location" id="location-search" value={this.state.location} onChange={this.handleInputChange}>
+                        {myArray.map(team => 
+                            <option value={team.location} key={team.id}>{team.location}</option>    
+                        )}
+                    </select>
+                    <label>Choose a Sport: </label>
+                    <select name="sport" value={this.state.sport} onChange={this.handleInputChange}>
+                        {myArray.map(team => 
+                            <option value={team.sport} key={team.id}>{team.sport}</option>    
+                        )}
+                    </select>
+                    <button type="submit">Search</button>
+                </form>
+                <div id="search-results">
+                    {searchTeams.map(team => {
+                        return(
+                            <div key={team.id}>
+                                <h2>{team.name}</h2>
+                                <h5>{team.location}</h5>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         )
     }
