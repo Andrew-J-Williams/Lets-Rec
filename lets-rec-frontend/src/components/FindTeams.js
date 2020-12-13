e5e848e04f2d71ece75fef6732ff0102625166cc
Andrew-J-Williams <1williamsaj@gmail.com>
2020-12-12 23:38:12 -0600

    rearranging divs on TeamsPage, creating select search under TeamFinder

--- lets-rec-frontend/src/components/FindTeams.js
+++ lets-rec-frontend/src/components/FindTeams.js
@@ -4,17 +4,34 @@ import '../FindTeams.css'
 
 class FindTeams extends React.Component {
 
+    state = {
+        location: '',
+        sport:''
+    }
+
+    handleInputChange = event => {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
     render(){
         const myArray = this.props.teams
-
+        console.log(this.state)
         return(
             <div className="find-teams-container">
-                {myArray.map(element => 
-                    <div key={element.id}>
-                        <h3>{element.name}</h3>
-                        <h4>{element.location}</h4>
-                    </div>    
-                )}
+                <h2>Find a Team!</h2>
+                <label for="locations">Choose a Location: </label>
+                <select name="location" id="location-search" onChange={this.handleInputChange}>
+                    {myArray.map(team => 
+                        <option value={team.location} key={team.id}>{team.location}</option>    
+                    )}
+                </select>
+                <label>Choose a Sport: </label>
+                <select name="sport" onChange={this.handleInputChange}>
+                {myArray.map(team => 
+                        <option value={team.sport} key={team.id}>{team.sport}</option>    
+                    )}
+                </select>
+                <button type="submit">Search</button>
             </div>
         )
     }
--- lets-rec-frontend/src/components/UserTeams.js
+++ lets-rec-frontend/src/components/UserTeams.js
@@ -17,13 +17,32 @@ class UserTeams extends React.Component {
 
     return(
         <div className="user-teams-container">
-            <h2 className="user-teams-title">My Teams</h2>
+            <h2 className="user-teams-title">Teams</h2>
             {this.props.teams.map(team => {
                 if (userTeamsArray.includes(team.id)){
                     console.log(team)
+                    let backSRC = ''
+
+                    if(team.sport === "Basketball"){
+                        backSRC = "https://i.imgur.com/4bZ7Q4f.png"
+                    }
+                    else if (team.sport === "Volleyball"){
+                        backSRC = "https://i.imgur.com/1CaO0Ad.png"
+                    }
+                    else if (team.sport === "Football"){
+                        backSRC = "https://i.imgur.com/k6cGaRb.png"
+                    }
+                    else if (team.sport === "Baseball"){
+                        backSRC = "https://i.imgur.com/YGeHFxs.png"
+                    }
+                    else {
+                        backSRC = "https://www.goyder.sa.gov.au/__data/assets/image/0030/544773/sports.jpg"
+                    }
+
                     return(
                     <div key={team.id} className="team-container">
                         <div className="team-circle">
+                            <img src={backSRC} className="team-background" alt="background"/>
                             <Link to={`teams/${team.id}`}><h3 className="team-name">{team.name}</h3></Link>
                             <h5 className="team-classification">{team.sport} League • {team.members} members</h5>
                         </div>
