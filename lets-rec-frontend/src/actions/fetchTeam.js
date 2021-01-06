bdc3409db326a29619ef5a098d123dcc706eeffe
Andrew-J-Williams <1williamsaj@gmail.com>
2021-01-05 21:55:50 -0600

    added extra restrictions to viewing TeamPages through fetchTeam

--- lets-rec-frontend/src/actions/fetchTeam.js
+++ lets-rec-frontend/src/actions/fetchTeam.js
@@ -3,8 +3,18 @@ export function fetchTeam(teamId){
         fetch(`http://localhost:3000/api/v1/teams/${teamId}`)
         .then(response => response.json())
         .then(data => {
-            dispatch(getTeam(data))
-            console.log(data)
+            let userId = []
+            data.users.forEach(user => {
+                if(user.id === parseInt(localStorage.currentUser, 10)){
+                    console.log("My Team!")
+                    userId.push(user.id)
+                }
+            })
+            if(userId.includes(parseInt(localStorage.currentUser, 10))){
+                dispatch(getTeam(data))
+            } else {
+                window.location.replace('http://localhost:3001/teams')
+            }
         })
     }
 }
